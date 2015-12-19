ed39471a12f802d73c3c5e1ee342d7038c502331
Bryan Bumgardner <bryanbumgardner@gmail.com>
2015-12-19 14:46:21 -0600

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,31 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+from datetime import datetime
+date_format = "%m-%d-%Y"
+a = datetime.strptime('01-02-2013', date_format)
+b = datetime.strptime('07-28-2015', date_format)
+jinkies = b - a
+jinkies.days
+
 ####b)  
 date_start = '12312013'  
 date_stop = '05282015'  
 
+from datetime import datetime
+date_format = "%m%d%Y"
+a = datetime.strptime('12312013', date_format)
+b = datetime.strptime('05282015', date_format)
+zoinks = b - a
+zoinks.days
+
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+from datetime import datetime
+date_format = "%d-%b-%Y"
+a = datetime.strptime('15-Jan-1994', date_format)
+b = datetime.strptime('14-Jul-2015', date_format)
+gang = b - a
+gang.days
